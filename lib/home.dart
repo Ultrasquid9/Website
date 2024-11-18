2555eb9632c3bb76a81f064bd81e03933cb565ec
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-11-18 16:53:28 -0500

    Carousel

--- lib/home.dart
+++ lib/home.dart
@@ -13,6 +13,26 @@ class _HomeState extends State<Home> {
 		return createPage(
 			context, 
 			[
+				Center(child: Text(
+					"Welcome to the Celeste Transgender Website!\n",
+					style: TextStyle(
+						fontSize: 24,
+						color: Color.fromARGB(255, 198, 208, 245)
+					),
+				)),
+                ConstrainedBox(
+                    constraints: const BoxConstraints(maxHeight: 300),
+                    child: CarouselView(
+                        itemExtent: 400, 
+                        children: [
+                            Image.network("https://maddymakesgames.com/articles/is_maddy_trans/images/header.webp", fit: BoxFit.cover),
+                            Image.network("https://maddymakesgames.com/articles/is_maddy_trans/images/farewell.webp", fit: BoxFit.cover),
+                            Image.network("https://maddymakesgames.com/articles/is_maddy_trans/images/call.webp", fit: BoxFit.cover),
+                            Image.network("https://maddymakesgames.com/articles/is_maddy_trans/images/selfieB.webp", fit: BoxFit.cover),
+                            Image.network("https://maddymakesgames.com/articles/is_maddy_trans/images/trans.webp", fit: BoxFit.cover)
+                        ]
+                    ),
+                ),
 				SelectableText(
 					text(), 
 					style: getTextStyle(),
